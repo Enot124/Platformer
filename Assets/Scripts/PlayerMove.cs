df513d203df7058be4572673d80778b203c37e5e
Enot <dan.enot@gmail.com>
2022-08-30 00:42:30 +0300

    MoveController and new Graphics

--- Assets/Scripts/PlayerMove.cs
+++ Assets/Scripts/PlayerMove.cs
@@ -2,8 +2,11 @@ using UnityEngine;
 
 public class PlayerMove : MonoBehaviour
 {
-   private float _directionX;
+   [SerializeField] private Transform _groundCheck;
+   [SerializeField] private LayerMask _groundLayer;
    private float _speed = 5f;
+   private float _jumpForce = 7f;
+   private bool _isGrounded;
    private Vector3 _direction;
    private Animator _animator;
    private Rigidbody2D _rigidbody;
@@ -16,34 +19,37 @@ public class PlayerMove : MonoBehaviour
 
    private void Update()
    {
-      if (Input.GetMouseButtonDown(0))
-      {
-         Attack();
-      }
-
-      if (Input.GetKeyDown(KeyCode.Space))
-      {
+      _isGrounded = CheckGround();
+      if (Input.GetKeyDown(KeyCode.Space) && _isGrounded)
          Jump();
-      }
-   }
 
-   private void FixedUpdate()
-   {
-      _direction.x = Input.GetAxisRaw("Horizontal");
+      _animator.SetBool("isJump", !_isGrounded);
+
+
+      _direction.x = (int)Input.GetAxisRaw("Horizontal");
       if (_direction.x != 0)
       {
          Move();
       }
       else
          _animator.SetBool("isMove", false);
+
+      if (Input.GetMouseButtonDown(0))
+      {
+         Attack();
+      }
+   }
+
+   private void FixedUpdate()
+   {
+
    }
 
    private void Move()
    {
-      _rigidbody.MovePosition(transform.position + _direction * _speed * Time.fixedDeltaTime);
-      //_rigidbody.velocity = new Vector2(_direction.x * _speed, _rigidbody.velocity.y);
+      _animator.SetBool("isMove", _isGrounded);
+      _rigidbody.velocity = new Vector2(_direction.x * _speed, _rigidbody.velocity.y);
       transform.localScale = new Vector3(_direction.x, transform.localScale.y, transform.localScale.z);
-      _animator.SetBool("isMove", true);
    }
 
    private void Attack()
@@ -53,7 +59,14 @@ public class PlayerMove : MonoBehaviour
 
    private void Jump()
    {
-      //_rigidbody.velocity.y =
+      _rigidbody.AddForce(transform.up * _jumpForce, ForceMode2D.Impulse);
+   }
+
+   private bool CheckGround()
+   {
+      Collider2D[] collider = Physics2D.OverlapCircleAll(_groundCheck.position, 0.3f, _groundLayer);
+      Debug.Log(collider.Length);
+      return collider.Length > 0;
    }
 
 
